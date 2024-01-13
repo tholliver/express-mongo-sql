70c30efec97b6934c4155893dba816b03ad1ad8c
tholliver <michaelmmv@outlook.com>
2024-01-13 16:39:19 -0400

    added production domain

--- cors.js
+++ cors.js
@@ -4,6 +4,7 @@ const ACCEPTED_ORIGINS = [
   'http://localhost:8080/',
   'http://localhost:3001/',
   'http://localhost:3000',
+  'https://dvdrental-site.vercel.app',
 ]
 export const corsMiddleware = ({ aceptedOrigins = ACCEPTED_ORIGINS } = {}) =>
   cors({
